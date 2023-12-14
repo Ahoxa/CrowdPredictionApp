297d27dd8cd5898498841bcb338089172eae780b
unknown <Ahoxa>
2023-12-14 11:50:08 +0900

    Add page transition action to calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { useRouter } from "next/navigation";
 
-function Calender() {
-  return <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />;
-}
+const Calender: React.FC = () => {
+  const router = useRouter();
+
+  const handleDateClick = (arg: any) => {
+    const dateStr = arg.dateStr;
+    router.push(`${dateStr}`);
+  };
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      dateClick={handleDateClick}
+    />
+  );
+};
 
 export default Calender;
