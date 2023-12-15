aec8dd7cdc429eae9f3c4b96c119b28ec364ca13
hayashibara <yujinyuto1223@gmail.com>
2023-12-16 00:37:29 +0900

    change font from inter into Alegreya

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Alegreya } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const alegreya = Alegreya({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "混雑予測ボード",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>
+      <body className={alegreya.className}>
         <Navbar />
         {children}
       </body>
