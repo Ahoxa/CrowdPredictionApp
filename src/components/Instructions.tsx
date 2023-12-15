78b069c1b2e45cf49f4ac95b0c63092df67b159f
hayashibara <yujinyuto1223@gmail.com>
2023-12-16 01:43:20 +0900

    Add a warning statement to Instruction.tsx

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 function Instructions() {
   return (
-    <div className="container my-5 text-center">
+    <div className="container mt-5 text-center">
       <h2>In order to share tommorow's breakfast crowd,</h2>
       <h2>
         We would appreciate your cooporation in selecting and registering the 
         time you plan to visit the restaurant.
       </h2>
+      <h2 className="my-5 text-red-600">※ This is not a reservation system ※</h2>
     </div>
   );
 }
