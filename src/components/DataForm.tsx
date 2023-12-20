d37ad53023e18296588d1e284de17535ad97178f
hayashibara <yujinyuto1223@gmail.com>
2023-12-20 14:45:46 +0900

    add room number input form and dialog

--- src/components/DataForm.tsx
+++ src/components/DataForm.tsx
@@ -4,6 +4,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import {
   Form,
   FormControl,
@@ -19,14 +21,24 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const roomErrorMessage: string = "Room number must be 3 digits.";
+const peopleNumErrorMessage: string = "Please enter the number of people.";
 const timeErrorMessage: string = "Please select your visiting time.";
 
 const schema = z.object({
   roomNumber: z.string().regex(/^\d{3}$/, { message: roomErrorMessage }),
+  numberOfPeople: z
+    .string()
+    .regex(/^\d{1,2}$/, { message: peopleNumErrorMessage }),
   time: z.string().nonempty({ message: timeErrorMessage }),
 });
 
@@ -35,6 +47,7 @@ export default function DataForm() {
     resolver: zodResolver(schema),
     defaultValues: {
       roomNumber: "",
+      numberOfPeople: "",
       time: "",
     },
   });
@@ -51,15 +64,38 @@ export default function DataForm() {
           name="roomNumber"
           render={({ field }) => (
             <FormItem>
-              <div className="flex">
-                <FormLabel htmlFor="roomNumber" className="mr-5 text-3xl whitespace-nowrap">
+              <div className="flex my-3">
+                <FormLabel
+                  htmlFor="roomNumber"
+                  className="mr-5 text-3xl whitespace-nowrap"
+                >
                   Room Number
                 </FormLabel>
                 <FormControl>
                   <Input placeholder="123" {...field} className="text-2xl" />
                 </FormControl>
               </div>
+              <FormMessage className="text-lg" />
+            </FormItem>
+          )}
+        />
 
+        <FormField
+          control={form.control}
+          name="numberOfPeople"
+          render={({ field }) => (
+            <FormItem>
+              <div className="flex">
+                <FormLabel
+                  htmlFor="numberOfPeople"
+                  className="mr-5 mb-4 text-3xl whitespace-nowrap"
+                >
+                  Number of People
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="1" {...field} className="text-2xl" />
+                </FormControl>
+              </div>
               <FormMessage className="text-lg" />
             </FormItem>
           )}
@@ -70,7 +106,7 @@ export default function DataForm() {
           name="time"
           render={({ field }) => (
             <FormItem>
-              <div className="flex">
+              <div className="flex mb-3">
                 <FormLabel
                   htmlFor="visitingTime"
                   className="mr-5 text-3xl whitespace-nowrap"
