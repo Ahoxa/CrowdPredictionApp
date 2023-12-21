c9e61f642b7bf69f63fb49ba4013e3f85af3ca89
unknown <Ahoxa>
2023-12-21 09:37:56 +0900

    update LangSelector to add 9 languages

--- src/components/LangSelector.tsx
+++ src/components/LangSelector.tsx
@@ -7,19 +7,34 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
+const languages = [
+  { value: "en", label: "English" },
+  { value: "fr", label: "Français" },
+  { value: "de", label: "Deutsch" },
+  { value: "es", label: "Español" },
+  { value: "it", label: "Italiano" },
+  { value: "pt", label: "Português" },
+  { value: "ru", label: "Русский" },
+  { value: "zh", label: "中文" },
+  { value: "ja", label: "日本語" },
+  { value: "ko", label: "한국어" },
+  { value: 'in', label: 'Bahasa Indonesia' },
+];
+
 function LangSelector() {
   return (
     <div className="flex justify-end mt-2 mr-5 md:text-3xl">
-      <div className="inline-block ">
+      <div className="inline-block">
         <Select>
           <SelectTrigger className="md:text-xl">
             <SelectValue placeholder="English" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="en" className="md:text-xl">
-              English
-            </SelectItem>
-            <SelectItem value="ja">日本語</SelectItem>
+            {languages.map((lang) => (
+              <SelectItem key={lang.value} value={lang.value}>
+                {lang.label}
+              </SelectItem>
+            ))}
           </SelectContent>
         </Select>
       </div>
